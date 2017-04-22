cdd797848b27c4048bae18f7a50cee962a58db25
Ian de Jesus <ian.dejesus.md@gmail.com>
2017-04-22 10:57:06 +0800

    Allocate enough size for all input:

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,52 +14,63 @@ public class Main {
         try (FileOutputStream binFile = new FileOutputStream("data.dat");
              FileChannel fileChannel = binFile.getChannel()) {
 
+            ByteBuffer buffer = ByteBuffer.allocate(100);
             byte[] outputBytes = "Hello World".getBytes();
-            //wrap method does 3 things
-            //1. allocates the size of the buffer
-            //2. copies the argument i.e. outputbytes into the buffer
-            //3. resets the position of buffer 0
-            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
-            int numBytes = fileChannel.write(buffer);
-            System.out.println("numbytes written " + numBytes);
+            buffer.put(outputBytes);
+            buffer.putInt(245);
+            buffer.putInt(896);
+            byte[] outBytes2 = "Foo bar".getBytes();
+            buffer.put(outBytes2);
+            buffer.putInt(1000);
+            buffer.flip();
+            fileChannel.write(buffer);
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            numBytes = fileChannel.write(intBuffer);
-            System.out.println("numbytes written " + numBytes);
-
-            intBuffer.flip();
-            intBuffer.putInt(896);
-            intBuffer.flip();
-            numBytes = fileChannel.write(intBuffer);
-            System.out.println("numbytes written " + numBytes);
-
+//            byte[] outputBytes = "Hello World".getBytes();
+//            //wrap method does 3 things
+//            //1. allocates the size of the buffer
+//            //2. copies the argument i.e. outputbytes into the buffer
+//            //3. resets the position of buffer 0
+//            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+//            int numBytes = fileChannel.write(buffer);
+//            System.out.println("numbytes written " + numBytes);
+//
+//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            intBuffer.putInt(245);
+//            intBuffer.flip();
+//            numBytes = fileChannel.write(intBuffer);
+//            System.out.println("numbytes written " + numBytes);
+//
+//            intBuffer.flip();
+//            intBuffer.putInt(896);
+//            intBuffer.flip();
+//            numBytes = fileChannel.write(intBuffer);
+//            System.out.println("numbytes written " + numBytes);
+//
+////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+////            FileChannel channel = ra.getChannel();
+////            long numBytesRead = channel.read(buffer);
+////            buffer.flip();
+////            if(buffer.hasArray()){
+////                System.out.println("byte buffer " + new String(buffer.array()));
+////            }
+//
 //            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
 //            FileChannel channel = ra.getChannel();
-//            long numBytesRead = channel.read(buffer);
+////            outputBytes[0] = 'a';
+////            outputBytes[1] = 'b';
 //            buffer.flip();
-//            if(buffer.hasArray()){
-//                System.out.println("byte buffer " + new String(buffer.array()));
+//            long numBytesRead = channel.read(buffer);
+//            System.out.println("numbytesread" + numBytesRead);
+//            if (buffer.hasArray()) {
+//                System.out.println("byte buffer = " + new String(buffer.array()));
 //            }
-
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-            FileChannel channel = ra.getChannel();
-//            outputBytes[0] = 'a';
-//            outputBytes[1] = 'b';
-            buffer.flip();
-            long numBytesRead = channel.read(buffer);
-            System.out.println("numbytesread" + numBytesRead);
-            if (buffer.hasArray()) {
-                System.out.println("byte buffer = " + new String(buffer.array()));
-            }
-
-            intBuffer.flip();
-            numBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
-            intBuffer.flip();
-            numBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
+//
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
+//            intBuffer.flip();
+//            numBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
 
 //            relative read
 //            intBuffer.flip();
@@ -71,9 +82,9 @@ public class Main {
 //            numBytesRead = channel.read(intBuffer);
 //            intBuffer.flip();
 //            System.out.println(intBuffer.getInt());
-
-            channel.close();
-            ra.close();
+//
+//            channel.close();
+//            ra.close();
 
 
         } catch (FileNotFoundException e) {
